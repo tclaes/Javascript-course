98e0e14bdd10b56aa0559be24c16e115f122f511
z411419 <tom.claes82@gmail.com>
2017-02-24 15:38:25 +0100

    image gallery

--- imagegallery/opdracht/JS_taak_displayImages.js
+++ imagegallery/opdracht/JS_taak_displayImages.js
@@ -4,56 +4,53 @@ window.onload = function(){
 
 	var eKnop = document.getElementById('hoofdknop');
 	var eImgs = document.querySelectorAll('.toggle');
-	var eButtons = document.getElementsByTagName('button');
 
+	// Array voor talen
 	var aKnoppen = [
-		["Sluiten", "Openen"], 
-		["Fermer", "Ouvrir" ]
+		[nederlands = ["Sluiten", "Openen"]], 
+		[frans = ["Fermer", "Ouvrir" ]]
 	];
 
+	// veranderen van taal
+	// 0 = nederlands
+	// 1 = frans
 	var language = aKnoppen[0];
 
-	console.log(language);
-
-	addButtons(eImgs, languageClose);
+	addButtons(eImgs, language);
 	
 	eKnop.addEventListener('click', function(e){
 		e.preventDefault();
-		hideImgs(eImgs);
+		hideImgs(eImgs, language);
 	});
 
 	document.addEventListener("click", function(event){
         var targetElement = event.srcElement;
         var eHide = targetElement.previousSibling.previousSibling;
-        console.log(targetElement.className);
-        if(targetElement.className === "btn" && targetElement.innerHTML === language){
+        //console.log(targetElement.className);
+        if(targetElement.className === "btn" && targetElement.innerHTML === language[0][0]){
         	var eHide = targetElement.previousSibling.previousSibling;
         eHide.style.display = "none";
-        targetElement.innerHTML = "Openen";
+        targetElement.innerHTML = language[0][1];
         //console.log(targetElement.previousSibling.previousSibling);
         }
-        else if(targetElement.innerHTML === "Openen"){
+        else if(targetElement.innerHTML === language[0][1]){
         	eHide.style.display = "block";
-        	targetElement.innerHTML = "Sluiten";
+        	targetElement.innerHTML = language[0][0];
         }
     });
-	
-
-	
-	
-
-
-	
-
-	
+		
 }
 
-function hideImgs(eImgs){
+function hideImgs(eImgs, language){
 
 	aImgs = eImgs.length;
+	console.log(language);
 
 	for(var i=0; i<aImgs; i++){
 		eImgs[i].style.display="none";
+		var button = document.getElementById('knop' + i);
+		console.log(button.id);
+		button.innerHTML= language[0][1];
 	}
 }
 
@@ -61,12 +58,9 @@ function addButtons(eImgs, language){
 
 	for (var i=0; i<eImgs.length; i++){
 		var button = document.createElement("button");
-		button.innerHTML = language;
+		button.innerHTML = language[0][0];
 		button.id = "knop" + i;
 		button.className = "btn";
 		eImgs[i].parentNode.appendChild(button);
-		//alert(eImgs[i]);
 	};
-
-	//console.log(eImgs.length);
 }
